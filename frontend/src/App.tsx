37929782f150d32bc1c2280c464e4a1392d69b2e
Dan Bennett <danbenn@umich.edu>
2023-05-13 18:27:31 +0200

    add farm overview skeleton for operator side

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
 import './App.css';
+import { OperatorDashboard } from "./pages/OperatorDashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/farms/:farmID",
+    element: <OperatorDashboard />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
   );
 }
 
