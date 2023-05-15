717a80848c67a5bdbf4c7482a75a2b7acdc2d1ea
Dan Bennett <danbenn@umich.edu>
2023-05-15 11:19:48 +0200

    fix module error by linking to typeorm

--- backend/src/panel/panel.module.ts
+++ backend/src/panel/panel.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PanelService } from './panel.service';
 import { PanelController } from './panel.controller';
+import { Panel } from './panel.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Panel])],
   controllers: [PanelController],
-  providers: [PanelService]
+  providers: [PanelService],
 })
 export class PanelModule {}
